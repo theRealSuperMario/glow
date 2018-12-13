9599158a24cf1a6ba9f0097d4c78c16cca959514
therealsupermario <supermario94123@gmail.com>
2018-12-13 08:44:07 +0100

    launcher for openaiglow

--- launcher_cifar.sh
+++ launcher_cifar.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-
-python -u train.py --problem cifar10 --pmap 1 --n_batch_train 32 --optimizer adam --lr 1e-4 --image_size 32 --depth 32 --n_levels 1 --width 400 --flow_permutation 2 --flow_coupling 1 --n_train 640 --seed 42
+python -u train.py \
+--problem cifar10 \
+--n_batch_init 32 \
+--pmap 1 \
+--n_batch_train 32 \
+--n_batch_init 32 \
+--n_batch_test 32 \
+--optimizer adam \
+--lr 1e-4 \
+--image_size 32 \
+--depth 26 \
+--n_levels 2 \
+--width 512 \
+--flow_permutation 2 \
+--flow_coupling 1 \
+--n_train 5000 \
+--seed 42 \
+--logdir logs \
+--dal 1 \
+--epochs_warmup 5
+&> train_out.txt
